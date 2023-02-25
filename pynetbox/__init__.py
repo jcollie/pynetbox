f051085a4051a98565c08b9646d5b2d69fe1739e
Jeffrey C. Ollie <jeff@ocjtech.us>
2023-02-25 16:54:11 -0600

    don't rely on pkg_resources being available

--- pynetbox/__init__.py
+++ pynetbox/__init__.py
@@ -1,9 +1,11 @@
-from pkg_resources import get_distribution, DistributionNotFound
-
 from pynetbox.core.query import RequestError, AllocationError, ContentError
 from pynetbox.core.api import Api as api
 
 try:
+    from pkg_resources import get_distribution, DistributionNotFound
+
     __version__ = get_distribution(__name__).version
+except ModuleNotFoundError:
+    pass
 except DistributionNotFound:
     pass
